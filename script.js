b633ef76434710211adf347abf592c4cb17d1b79
Abhishek-c0der <your-email@example.com>
2025-04-15 12:30:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Welcome")
+console.log("Hello from script.js!");
+
+let name = "Abhishek";
+console.log("My name is " + name);
+
+let a = 5;
+let b = 10;
+console.log("Sum of a and b is:", a + b);
